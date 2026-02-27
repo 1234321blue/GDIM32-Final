adb45998c4416f5a4d2ade819147e8f67717ba2d
gaelp-io <gandresp20@gmail.com>
2026-02-26 21:08:34 -0800

    added interactable milk item

--- A Small Favor (GDIM 32 Final)/Assets/Scripts/PlayerController.cs
+++ A Small Favor (GDIM 32 Final)/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,10 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float mouseSensitivity = 200f;
+    public float interactRange = 3f;
+    public Transform holdPoint;
+
+    private GameObject heldItem;
 
     public Transform playerCamera;
 
@@ -21,6 +25,16 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         HandleMouseLook();
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            TryPickup();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            DropItem();
+        }
     }
 
     void FixedUpdate()
@@ -54,5 +68,55 @@ public class PlayerController : MonoBehaviour
             rb.velocity.y,
             moveDirection.z * moveSpeed
         );
-}
+    }
+    
+    void TryPickup()
+    {
+        if (heldItem != null) return;
+
+        Ray ray = new Ray(playerCamera.position, playerCamera.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, interactRange))
+        {
+            if (hit.collider.CompareTag("item"))
+            {
+                heldItem = hit.collider.gameObject;
+
+            Rigidbody itemRb = heldItem.GetComponent<Rigidbody>();
+            if (itemRb != null)
+            {
+                itemRb.isKinematic = true;
+            }
+
+            heldItem.transform.SetParent(holdPoint);
+            heldItem.transform.localPosition = Vector3.zero;
+            heldItem.transform.localRotation = Quaternion.identity;
+
+            Collider col = heldItem.GetComponent<Collider>();
+            if (col != null)
+                col.enabled = false;
+            }
+        }
+    }   
+
+    void DropItem()
+    {
+        if (heldItem == null) return;
+
+        heldItem.transform.SetParent(null);
+
+        Collider col = heldItem.GetComponent<Collider>();
+        if  (col != null)
+            col.enabled = true;
+
+        Rigidbody itemRb = heldItem.GetComponent<Rigidbody>();
+        if (itemRb != null)
+        {
+            itemRb.isKinematic = false;
+            itemRb.AddForce(playerCamera.forward * 2f, ForceMode.Impulse);
+        }
+
+        heldItem = null;
+    }
 }
