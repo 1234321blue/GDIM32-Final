3410a75af01d5275460a4f1c4715e5fa00ec1610
1234321blue <djeremiahyang@gmail.com>
2026-03-02 21:34:50 -0800

    started dialogue and stuff

---
+++ A Small Favor (GDIM 32 Final)/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Dialogue", menuName = "ScriptableObjects/Dialogue", order = 1)]
+public class Dialogue : ScriptableObject
+{
+    public string[] npcDialogue;
+    public string[] playerResponses;
+    public Dialogue[] npcReplies;
+}
--- A Small Favor (GDIM 32 Final)/Assets/Scripts/NPC.cs
+++ A Small Favor (GDIM 32 Final)/Assets/Scripts/NPC.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+public enum Mood {happy, fine, mad}
 public class NPC : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Transform npcTransform;
+    [SerializeField] private float checkRad = 1f;
+    [SerializeField] private float checkDistance = 1f;
+    public Mood npcMood;
+    public int interactionChain;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        CheckForPlayer();
+    }
+
+    private void CheckForPlayer()
+    {
+        RaycastHit hit;
+        bool playerThere = Physics.SphereCast(npcTransform.position, checkRad,npcTransform.forward, out hit, checkDistance);
+        if (playerThere)
+        {
+            if (interactionChain==0)
+            {
+                
+            }
+            else if (interactionChain==1)
+            {
+                
+            }
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color=Color.red;
+        Gizmos.DrawWireSphere(npcTransform.position + npcTransform.forward * checkDistance, checkRad);
     }
 }
